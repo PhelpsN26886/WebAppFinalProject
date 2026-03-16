d9d84dad63f55ff80d680666ccff025a405e306e
PhelpsN26886 <nornorphelps@gmail.com>
2026-03-15 23:58:34 -0500

    Create logout.php

---
+++ logout.php
@@ -0,0 +1,13 @@
+<?php
+// logout.php – destroys the session and redirects home
+require_once 'config.php';
+
+$_SESSION = [];
+if (ini_get('session.use_cookies')) {
+    $p = session_get_cookie_params();
+    setcookie(session_name(), '', time() - 42000,
+        $p['path'], $p['domain'], $p['secure'], $p['httponly']);
+}
+session_destroy();
+
+redirect('homePage.php');
